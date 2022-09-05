ac37aaf55fd4b7398e1cc513bb263052680323d0
Geogory <Mohammad.azem@ymail.com>
2022-09-05 03:10:19 +0300

    Made some changes

--- My project 🔥/Assets/quit.cs
+++ My project 🔥/Assets/quit.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class quit : MonoBehaviour
 {
+    public static CursorLockMode lockState;
+
     void Start()
     {
         //Set Cursor to not be visible
         Cursor.visible = true;
+    }
+    void Update()
+    {
+        //Press the space bar to apply no locking to the Cursor
+        if (Input.GetKey(KeyCode.Space))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    void OnGUI()
+    {
+        //Press this button to lock the Cursor
+        if (GUI.Button(new Rect(0, 0, 100, 50), "Lock Cursor"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        //Press this button to confine the Cursor within the screen
+        if (GUI.Button(new Rect(125, 0, 100, 50), "Confine Cursor"))
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
     public void QuitGame()
     {
